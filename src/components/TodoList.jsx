7cc84702e6d3839b55d29a7ef59a4239ae32b1eb
AufaIzza <aufashidqiizza@gmail.com>
2024-04-12 09:51:30 +0700

    changing == to ===

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -13,28 +13,38 @@ function TodoList() {
                 <div id="Not-completed" className="TodosDiv">
                     <h2>Not Completed</h2>
                     { todos.map((e) => {
-                        if (e.completion == 0)
-                        return(
-                            <Todo key={e.id} todo={e.todo}/>
-                        )
+                        if (e.completion === 0) {
+                            return(
+                                <Todo key={e.id} todo={e.todo}/>
+                            )
+                        }
+                        else {
+                            return
+                        }
                     })}
                 </div>
                 <div id="In-progress" className="TodosDiv">
                     <h2>In Progress</h2>
                     { todos.map((e) => {
-                        if (e.completion == 1)
-                        return(
-                            <Todo key={e.id} todo={e.todo}/>
-                        )
+                        if (e.completion === 1) {
+                            return(
+                                <Todo key={e.id} todo={e.todo}/>
+                            )
+                        } else {
+                            return
+                        }
                     })}
                 </div>
                 <div id="Completed" className="TodosDiv">
                     <h2>Completed</h2>
                     { todos.map((e) => {
-                        if (e.completion == 2)
-                        return(
-                            <Todo key={e.id} todo={e.todo}/>
-                        )
+                        if (e.completion === 2) {
+                            return(
+                                <Todo key={e.id} todo={e.todo}/>
+                            )
+                        } else {
+                            return
+                        }
                     })}
                 </div>
             </div>
